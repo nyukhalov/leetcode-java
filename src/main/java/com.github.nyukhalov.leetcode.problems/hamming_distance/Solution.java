f82876636fcf08bd7237fb906f202001c21f0f7d
Roman Niukhalov <r.nyukhalov@gmail.com>
2018-01-14 13:54:16 +0700

    Fixed packages

---
+++ src/main/java/com.github.nyukhalov.leetcode.problems/hamming_distance/Solution.java
@@ -0,0 +1,13 @@
+package com.github.nyukhalov.leetcode.hamming_distance;
+
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int xor = x ^ y;
+        int res = 0;
+        for (int i=0; i<31; i++) {
+            int ithBit = 1 << i;
+            if ((xor & ithBit) != 0) res++;
+        }
+        return res;
+    }
+}
