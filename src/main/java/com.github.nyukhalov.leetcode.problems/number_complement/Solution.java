6058412ddaafc7e9332ed5b191b41fc80789de18
Roman Niukhalov <r.nyukhalov@gmail.com>
2018-01-20 15:29:03 +0700

    Solved 'Number Complement

---
+++ src/main/java/com.github.nyukhalov.leetcode.problems/number_complement/Solution.java
@@ -0,0 +1,19 @@
+package com.github.nyukhalov.leetcode.problems.number_complement;
+
+class Solution {
+    public int findComplement(int num) {
+        boolean facedNonZeroBit = false;
+        for (int offset = 31; offset >= 0; offset--) {
+            int bit = (1 << offset);
+            if ((num & bit) != 0) {
+                facedNonZeroBit = true;
+                num = num ^ bit;
+            } else {
+                if (facedNonZeroBit) {
+                    num = num | bit;
+                }
+            }
+        }
+        return num;
+    }
+}
