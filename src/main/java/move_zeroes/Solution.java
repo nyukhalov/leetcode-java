b368715c0adab7b404992153d7bbfe6243edd3e6
Roman Niukhalov <r.nyukhalov@gmail.com>
2018-03-25 17:55:32 +0700

    Use optimal solution from forum

--- src/main/java/move_zeroes/Solution.java
+++ src/main/java/move_zeroes/Solution.java
@@ -3,31 +3,11 @@ package move_zeroes;
 // https://leetcode.com/problems/move-zeroes/description/
 class Solution {
     public void moveZeroes(int[] nums) {
-        int zeroIdx = 0;
-        int lastNonZeroIdx = findFirstNonZeroBackwards(nums);
-        for (;;) {
-            zeroIdx = findFirstZero(nums, zeroIdx);
-            if (zeroIdx >= lastNonZeroIdx) return;
-            move(nums, zeroIdx, lastNonZeroIdx);
-            lastNonZeroIdx--;
-        }
-    }
-
-    private int findFirstZero(int[] nums, int startedFrom) {
-        int i = startedFrom;
-        while (i < nums.length - 1 && nums[i] != 0) i++;
-        return i;
-    }
-
-    private int findFirstNonZeroBackwards(int[] nums) {
-        int i = nums.length - 1;
-        while(i > 0 && nums[i] == 0) i--;
-        return i;
-    }
-
-    private void move(int[] nums, int from, int to) {
-        for (int i=from; i<to; i++) {
-            swap(nums, i, i + 1);
+        for (int lastNonZeroFoundAt = 0, cur = 0; cur < nums.length; cur ++) {
+            if (nums[cur] != 0) {
+                swap(nums, lastNonZeroFoundAt, cur);
+                lastNonZeroFoundAt++;
+            }
         }
     }
 
